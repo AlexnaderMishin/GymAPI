fbb787f6b44935ada112dc24671c15fe31925d33
Alexander <alexander.mishin.2001@gmail.com>
2022-12-19 13:34:30 +0300

    занесение характеристик в БД

--- back/back.php
+++ back/back.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 header('Content-Type: application/json; charset=utf-8');
-
+session_start();
 $data = $_POST["data"];
 $data = json_decode($data, TRUE);
 
@@ -10,5 +10,13 @@ $h = $data["height"];
 $w = $data["weight"];
 $indexMass = $data["imb"];
 $indexFat = $data["ifb"];
+$userId = $_SESSION['userId'];
 
-echo $h.' '.$w.' '.$indexMass;
+if( isset ($h, $w, $indexMass)){
+    
+    $sql = "INSERT INTO user_info (`user_id`, `user_weight`, `user_height`, `ibm`) VALUES ('$userId','$w', '$h', '$indexMass')";
+    $pdo->exec($sql);
+    echo $h.'-'.$w.'-'.$indexMass.'-'.$_SESSION['userId'];
+}
+
+// echo $h.' '.$w.' '.$indexMass;
